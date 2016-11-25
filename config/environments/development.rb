11d3c62c5ed6d193b4948f108fb0144f2283ca9b
ed-karabinus <ejkarabin@gmail.com>
2016-11-25 15:59:29 -0600

    Added Devise and Facebook omniauth for User authentication; also created WelcomeController

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Need default url options for Devise
+  # In production, :host should be set to the actual host of the application
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
