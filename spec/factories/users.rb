dae946a2940a07111be5bc4fcc3ba34c31a8b4fa
ed-karabinus <ejkarabin@gmail.com>
2016-12-24 12:02:21 -0600

    Currently trying to integrate FactoryGirl and Devise into my project. No specs passing.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,14 @@ FactoryGirl.define do
   factory :user do
     name "Test User"
     email "test_user@example.com"
-    password "password123"
+  end
+end
+
+FactoryGirl.modify do
+  factory :user do
+    after(:build) { |u| 
+      u.password = "password123" 
+      u.password_confirmation = "password123"
+    }
   end
 end
