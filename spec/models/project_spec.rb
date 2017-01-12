cc8b03f493b8552f3ba92dd0b50f0874cb2bad0f
ed-karabinus <ejkarabin@gmail.com>
2017-01-12 14:02:46 -0600

    Added project model specs to spec/models/project_spec.rb. All tests passing.

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Project, :type => :model do
+  let!(:project) { FactoryGirl.create(:project) }
+  let!(:user) { FactoryGirl.create(:user) }
+
+  it 'has sessions' do
+    expect(project).to respond_to(:sessions)
+  end
+
+  it 'has users' do
+    expect(project).to respond_to(:users)
+  end
+
+  it 'has comments' do
+    expect(project).to respond_to(:comments)
+  end
+
+  it 'validates the presence of name upon creation' do
+    expect { FactoryGirl.create(:project, :name => "") }.to raise_error(/Name can't be blank/)
+  end
+
+  it 'validates the presence of name on update' do
+    expect(project.update(:name => "")).to be false
+  end
+
+  it 'has a nested attribute setter called #user_attributes=' do
+    expect(project).to respond_to(:users_attributes=)
+    project.users_attributes=({id: User.first.id})
+    expect(project.users).to include(User.first)
+  end
+end
