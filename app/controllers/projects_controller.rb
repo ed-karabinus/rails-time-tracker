95f352e2d30aa5f112077502a5e8f879f999829e
ed-karabinus <ejkarabin@gmail.com>
2017-01-29 12:52:46 -0500

    Added ProjectsController#edit and ProjectsController#update; added view app/views/projects/edit.html.erb; updated app/views/welcome/index.html.erb and app/views/layouts/application.html.erb.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,6 +19,22 @@ class ProjectsController < ApplicationController
     @project = Project.find_by(id: params[:id])
   end
 
+  def update
+    @project = Project.find_by(id: params[:id])
+    if @project.update(project_params)
+      if !@project.users.include?(current_user)
+        @project.users << current_user
+      end
+      redirect_to project_path(@project)
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @project = Project.find_by(id: params[:id])
+  end
+
   def project_params
     params.require(:project).permit(:name, :description, :users_attributes => [:email])
   end
